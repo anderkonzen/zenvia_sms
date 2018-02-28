709e308cdc7f5a2e6d724975fee671ff35259571
Anderson Konzen <anderson.konzen@gmail.com>
2018-02-28 18:06:31 -0300

    Project creation

---
+++ lib/zenvia_sms.ex
@@ -0,0 +1,18 @@
+defmodule ZenviaSms do
+  @moduledoc """
+  Documentation for ZenviaSms.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZenviaSms.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zenvia_sms_test.exs
@@ -0,0 +1,8 @@
+defmodule ZenviaSmsTest do
+  use ExUnit.Case
+  doctest ZenviaSms
+
+  test "greets the world" do
+    assert ZenviaSms.hello() == :world
+  end
+end
